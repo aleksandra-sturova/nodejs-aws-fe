1729204d9093d688d8e761ab3f896062572f89bd
Aleksandra Sturova <Aleksandra_Sturova@epam.com>
2020-12-13 16:06:02 +0300

    feat: task 8 cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://l2xwwcjprk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://y8cv6h0sql.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://aleksandra-sturova-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
